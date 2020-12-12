7f403cc1c710e3172cb19cf613482815f3d19f38
“anmiho” <1307426250@qq.com>
2020-12-12 17:25:25 +0800

    init

---
+++ src/main/java/com/mian/MianBlogApplication.java
@@ -0,0 +1,13 @@
+package com.mian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MianBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MianBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mian/MianBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MianBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
