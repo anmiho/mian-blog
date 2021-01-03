fa63f59cb2baed05abbb7649fe97e195cf3daa7d
mian <1307426250@qq.com>
2021-01-03 13:54:12 +0800

    1.Druid配置;

--- src/main/java/com/mian/config/DruidConfig.java
+++ src/main/java/com/mian/config/DruidConfig.java
@@ -1,6 +1,18 @@
 package com.mian.config;
 
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Description: Druid配置
@@ -9,4 +21,44 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class DruidConfig {
+
+    /**
+     * 绑定配置文件中的相关属性
+     */
+    @ConfigurationProperties(prefix = "spring.datasource")
+    @Bean
+    public DataSource druid(){
+        return new DruidDataSource();
+    }
+
+    /**
+     * 配置Druid的监控,配置一个管理后台的Servlet
+     */
+    @Bean
+    public ServletRegistrationBean statViewServlet(){
+        ServletRegistrationBean bean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
+        Map<String,String> initParameters = new HashMap<>();
+        // 登录Druid的用户名
+        initParameters.put("loginUsername","admin");
+        // 登录Druid的密码
+        initParameters.put("loginPassword","mian");
+        //默认就是允许所有访问
+        initParameters.put("allow","");
+        bean.setInitParameters(initParameters);
+        return bean;
+    }
+
+    /**
+     * 配置一个web监控的filter
+     */
+    @Bean
+    public FilterRegistrationBean webStatFilter(){
+        FilterRegistrationBean bean = new FilterRegistrationBean();
+        bean.setFilter(new WebStatFilter());
+        Map<String,String> initParameters = new HashMap<>();
+        initParameters.put("exclusions","*.js,*.css,/druid/*");
+        bean.setInitParameters(initParameters);
+        bean.setUrlPatterns(Arrays.asList("/*"));
+        return bean;
+    }
 }
